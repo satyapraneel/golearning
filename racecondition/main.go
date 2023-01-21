5d73f47a1fe3c270de022ec49e1517f98bc7cb11
satyapraneel <satyapraneel@gmail.com>
2023-01-21 20:30:34 +0530

    race

--- racecondition/main.go
+++ racecondition/main.go
@@ -2,21 +2,46 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
-
+	wg := &sync.WaitGroup{}
+	mu := &sync.RWMutex{}
 	fmt.Println("race")
-	score := []int{0}
-	go func() {
+	var score = []int{0}
+	wg.Add(3)
+	// go func(wg *sync.WaitGroup, mu *sync.RWMutex) {
+	// 	mu.RLock()
+	// 	defer wg.Done()
+	// 	fmt.Println(score)
+	// 	mu.RUnlock()
+	// }(wg, mu)
+
+	go func(wg *sync.WaitGroup, mu *sync.RWMutex) {
 		fmt.Println("1")
+		mu.Lock()
+		defer mu.Unlock()
+		defer wg.Done()
 		score = append(score, 1)
-	}()
-	go func() {
+
+	}(wg, mu)
+	go func(wg *sync.WaitGroup, mu *sync.RWMutex) {
 		fmt.Println("2")
+		mu.Lock()
+		defer mu.Unlock()
+		defer wg.Done()
 		score = append(score, 2)
-	}()
-	go func() {
+	}(wg, mu)
+	go func(wg *sync.WaitGroup, mu *sync.RWMutex) {
 		fmt.Println("3")
-	}()
+		mu.RLock()
+		defer wg.Done()
+		score = append(score, 3)
+		mu.RUnlock()
+	}(wg, mu)
+
+	wg.Wait()
+	fmt.Println(score)
+
 }
